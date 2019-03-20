dbd4cea7b5ef7877167595dc63612f7dde93e818
浅忆梦微凉 <3538922590@qq.com>
2019-03-20 22:12:19 +0800

    直接插入排序

---
+++ 八大排序算法/test.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+//将一个记录插入到已排序好的有序表中，从而得到一个新，记录数增1的有序表。
+//即：先将序列的第1个记录看成是一个有序的子序列，
+//然后从第2个记录逐个进行插入，直至整个序列有序为止。
+
+//直接插入排序
+vector<int>  InsertSort(vector<int>  array){
+	int i = 0;
+	int temp = array[0];
+	for (i = 1; i < array.size(); ++i){
+		temp = array[i];
+		int j = i;
+		while (j > 0 && temp < array[j - 1]){
+			array[j] = array[j - 1];
+			j--;
+		}
+		array[j] = temp;
+	}
+	return array;
+}
+
+int main(){
+	vector<int> array = { 0, 4, 5, 3, 6, 7, 1, 8, 2, 9 };
+	array = InsertSort(array);
+	for (int i = 0; i < array.size(); ++i){
+		cout << array[i] << " " ;
+	}
+	system("pause");
+	return 0;
+}
