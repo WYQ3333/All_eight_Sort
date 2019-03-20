789df5f221dc0aba0675ee22f0b5fbc27b28ded3
浅忆梦微凉 <3538922590@qq.com>
2019-03-20 22:17:08 +0800

    冒泡排序

--- 八大排序算法/test.cpp
+++ 八大排序算法/test.cpp
@@ -22,12 +22,34 @@ vector<int>  InsertSort(vector<int>  array){
 	return array;
 }
 
+//ц╟ещеепР
+
+vector<int> BubbleSort(vector<int> array){
+	int i = 0;
+	for (i = 0; i < array.size(); ++i){
+		for (int j = i; j < array.size(); ++j){
+			if (array[j]<array[i]){
+				int temp = array[j];
+				array[j] = array[i];
+				array[i] = temp;
+			}
+		}
+	}
+	return array;
+}
+
 int main(){
 	vector<int> array = { 0, 4, 5, 3, 6, 7, 1, 8, 2, 9 };
-	array = InsertSort(array);
+	/*array = InsertSort(array);
 	for (int i = 0; i < array.size(); ++i){
 		cout << array[i] << " " ;
+	}*/
+	
+	array = BubbleSort(array);
+	for (int i = 0; i < array.size(); ++i){
+		cout << array[i] << " ";
 	}
+
 	system("pause");
 	return 0;
 }
